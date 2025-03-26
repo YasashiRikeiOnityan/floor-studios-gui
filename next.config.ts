6fe18e9c4915231dc8c219638b49185824343601
Minamiyama Riku <>
2025-03-26 18:25:20 +0900

    NextConfigを設定する

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+  basePath: "",
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://main.api.floor-studios.com/v1/:path*',
+        source: "/api/:path*",
+        destination: "https://main.api.floor-studios.com/v1/:path*",
       },
     ];
   },
