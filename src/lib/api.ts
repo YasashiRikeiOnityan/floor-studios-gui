304659173a6a3c27242396a65fd4ac9f7320a9d0
YasashiRikeiOnityan <nanshanlu524@gmail.com>
2025-03-30 19:51:21 +0900

    idTokenをストアから取得する

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,7 @@ const httpClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 })
 
-const idToken = "eyJraWQiOiJ5cndVNndyQTBWN2grRVZWWG5QVjhuOEhSeFg5NHhEMVhnRlZ2T0FuNmswPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI4N2E0MWEyOC0zMDcxLTcwMjUtZWY5YS1hZTYyODFkM2RkNGMiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLW5vcnRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLW5vcnRoZWFzdC0xX1ZZbDBabkJEWCIsImNvZ25pdG86dXNlcm5hbWUiOiI4N2E0MWEyOC0zMDcxLTcwMjUtZWY5YS1hZTYyODFkM2RkNGMiLCJjdXN0b206dGVuYW50X2lkIjoiOTllYTQ4MGEtN2VlMi00NzE4LWE0MzEtMjA5M2FjM2JlYjlhIiwib3JpZ2luX2p0aSI6IjI5OTljOGE3LTdjNzItNGI0Ni1hZTQzLTU1ZjBmODJkMWU2YyIsImF1ZCI6IjRuMXRmdnR1bGcyYmJpMWRhZ2IxNjQycm50IiwiZXZlbnRfaWQiOiI3Nzc5NTQ1MC1kZjU2LTRjZGYtYWRkZC03YzJiYTZmNGFmNGIiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTc0MzMxNjgyNCwiZXhwIjoxNzQzMzIwNDI0LCJpYXQiOjE3NDMzMTY4MjQsImp0aSI6IjEzM2YwNjg3LWVmMTAtNDk3Mi04ZmQzLWIxMGQxYzE5MjU5MiIsImVtYWlsIjoibmFuc2hhbmx1NTI0QGdtYWlsLmNvbSJ9.oLYlkFfVADEZVkmip76MBKqBUBRwkJ0jW4g9vb_hb6_IiOo1JVA26DR82Bhk3UolCuq4des754jyvvkTFdupi0Zc04GJFJ8h2D5ABHKP4RDC_BlY1bY2GYq7Uqj4nDBdizr3KFgu0XbxalONWybiJ0L4Y9Y2q4PcLSri9tGKfTGc0v2YyLAdOh62UInEAxix49oDNXSyd1sHc38MJ4pDkezFY5ScD_G-1-TwAssXjB_epFYY2hoPI6QjGhq4pthqA2v-Y0G2IJqwtSttnklOcjFtNP9ZD6Y7EHrdgFRjkxajeeuTGDLRUVpmPD4KCrXbEv3T5IHMfrkhazeXauYRqw";
+const idToken = authStore.getIdToken();
 
 export const ApiGetUsers = async () => {
   try {
@@ -14,6 +14,7 @@ export const ApiGetUsers = async () => {
     const response = await httpClient.get(url, {
       headers: {
         Authorization: `Bearer ${idToken}`,
+        "Access-Control-Allow-Origin": "*",
       }
     })
     console.log(response.data.body)
