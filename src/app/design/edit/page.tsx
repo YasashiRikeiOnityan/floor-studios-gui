5890cc9d13ff63b6776d658a4a29d4a2398bedec
Minamiyama Riku <>
2025-04-20 07:51:35 +0900

    t-shirtの画像を追加

--- src/app/design/edit/page.tsx
+++ src/app/design/edit/page.tsx
@@ -1,5 +1,25 @@
-const EditDesign = () => {
+import { useSearchParams } from "next/navigation";
+import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
+import { specificationStore } from "@/stores/specificationStore";
+
+const EditDesign = observer(() => {
+  const searchParams = useSearchParams();
+  const specificationId = searchParams.get("id") || "";
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (mounted && specificationId) {
+      // specificationStore.getSpecifications(specificationId);
+    }
+  }, [specificationId, mounted]);
+
   return <div>EditDesign</div>;
-};
+});
 
 export default EditDesign;
