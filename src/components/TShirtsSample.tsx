94e74aca645a0e32437d9a093e3bc01b10a60ffb
Minamiyama Riku <>
2025-05-08 19:19:09 +0900

    アポストロフィをエスケープした

--- src/components/TShirtsSample.tsx
+++ src/components/TShirtsSample.tsx
@@ -103,7 +103,7 @@ const TShirtsSample = (props: TShirtsSampleProps) => {
                 className="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden [&:not(:checked)]:before:hidden"
               />
               <label htmlFor="no" className="ml-3 block text-sm/6 font-medium text-gray-900">
-                No, I don't want a sample
+                No, I don&apos;t want a sample
               </label>
             </div>
           </div>
